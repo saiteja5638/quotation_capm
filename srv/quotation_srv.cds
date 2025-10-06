2cf06012721031fa55604c0429be98ed8800b571
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2025-10-06 11:13:53 +0000

    new chnages

--- srv/quotation_srv.cds
+++ srv/quotation_srv.cds
@@ -2,16 +2,7 @@ using app.quotation from '../db/quotation';
 
 
 service Quotation {
-    entity Z_QUOTATION      as projection on quotation.Z_QUOTATION;
-
-    action ApproveQuotation(DATA:String) returns String;
-    action predictQuotation(quotation:array of quotation.quotation_payload) returns quotation.Res; 
-
-    function createQuotation(KUNNR : String,
-                             VKORG : String,
-                             MATNR : String,
-                             NETPR : String, // Allowing for null values
-                             KWMENG : String) returns String; // You can change the return type as required
-
+    entity Z_QUOTATION as projection on quotation.Z_QUOTATION;
+    action predictQuotation(quotation: array of quotation.quotation_payload) returns quotation.Res;
 
 }
--- srv/quotation_srv.js
+++ srv/quotation_srv.js
@@ -1,31 +1,8 @@
 const cds = require('@sap/cds');
 const { DELETE } = require('@sap/cds/lib/ql/cds-ql');
 const axios = require('axios');
-var WebSocket = require('ws');
 
 module.exports = async (srv) => {
-    srv.on('createQuotation', async (req, res) => {
-        try {
-
-
-            let result = req.data.KUNNR.replace(/^00/, ''); // Remove the first two zeros
-
-            const requestData = {
-                KUNNR: result,
-                VKORG: parseInt(req.data.VKORG),
-                MATNR: req.data.MATNR,
-                NETPR: parseInt(req.data.NETPR),
-                KWMENG: parseInt(req.data.KWMENG)
-            };
-            const reqUrl = "http://100.96.8.237:8001/predict";
-            const response = await axios.post(reqUrl, requestData);
-            return response.data;
-        } catch (error) {
-            console.log(error);
-            req.reject(500, error.message);
-        }
-    });
-
     srv.on('predictQuotation',async(req,res)=>{
         try {
             let {KUNNR,VKORG,MATNR,NETPR,KWMENG} =  req.data.quotation[0];
@@ -40,7 +17,7 @@ module.exports = async (srv) => {
                 NETPR: parseInt(NETPR),
                 KWMENG: parseInt(KWMENG)
             };
-            const reqUrl = "https://41045a93trial-dev-myapp.cfapps.us10-001.hana.ondemand.com/predict";
+            const reqUrl = "http://100.96.27.87:8001/predict";
             const response = await axios.post(reqUrl, requestData);
             return {
                 Response:response.data
